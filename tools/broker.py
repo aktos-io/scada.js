c6565111d2fbc71294dad6a34d03b6ce9c5dfad4
cca <cca@aktos1>
2016-03-25 20:06:33 +0200

    tools are updated regarding to update in aktos_dcs
    Former-commit-id: a938b71986cae52545740b87cd6e243b815e9eb5
    Former-commit-id: cd32f901d6406ca31d73d5f0dbbc7edb5c695c4b
    Former-commit-id: ac7d7795d526bbc243f58df4fdcaf42421ba45d5
    Former-commit-id: c4c3347cd4471df64faa71b97169c8d486cafb1d [formerly ad982a653fa7af8c085f5c63b2898a78001c5f99]
    Former-commit-id: 6bc8e7a43b37f3d0a8dd7e2df808b91b5326f542

--- tools/broker.py
+++ tools/broker.py
@@ -1,9 +1,7 @@
 from aktos_dcs import *
-from aktos_dcs_lib import *
 
 class Monitor(Actor):
     def handle_IoMessage(self, msg):
-        msg = get_msg_body(msg)
         print "monitor got io message:", msg['pin_name'], msg['val']
 
 ProxyActor(proxy_brokers="localhost:9012:9013", brokers="10.0.10.176:5012:5013")
