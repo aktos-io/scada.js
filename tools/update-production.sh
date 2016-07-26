10c68eb78fa485b0f92fe63ae241054f5cd564c0
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2016-07-26 07:13:23 +0300

    update-production script fixed as submodules added

--- tools/update-production.sh
+++ tools/update-production.sh
@@ -1,10 +1,11 @@
-#!/bin/bash 
+#!/bin/bash
 
 CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$CURR_DIR/.."
 
 git pull 
-rm -r build 
+git submodule foreach --recursive git submodule update --init
+rm -r build
 gulp --compile
 echo "Copying build/public to __public__"
 rm -r __public__ && cp -a build/public/ __public__
