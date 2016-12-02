35682109670e448d898e9041336de2db3bbebf35
sultanDMTR <sultan_aktin@hotmail.com>
2016-12-02 13:54:21 +0300

    style added to panel-heading

--- vendor/ractive-plugins/ractive-bootstrap.js
+++ vendor/ractive-plugins/ractive-bootstrap.js
@@ -533,16 +533,17 @@ Ractive.components['panel'] = Ractive.extend({
 	data: {
 		type: 'default',
 		hidden: false,
+		style:'',
 		icon: ''
 	},
-	template: "<panel-custom type='{{type}}' hidden='{{hidden}}'>{{#title}}<panel-heading>{{#icon}}<icon name='{{icon}}'/> {{/icon}}{{title}}</panel-heading>{{/title}}<panel-body>{{yield}}</panel-body>{{#footer}}<panel-footer>{{footer}}</panel-footer>{{/footer}}</panel-custom>"
+	template: "<panel-custom type='{{type}}' hidden='{{hidden}}'>{{#title}}<panel-heading style='{{style}}'>{{#icon}}<icon name='{{icon}}'/> {{/icon}}{{title}}</panel-heading>{{/title}}<panel-body>{{yield}}</panel-body>{{#footer}}<panel-footer>{{footer}}</panel-footer>{{/footer}}</panel-custom>"
 })
 Ractive.components['panel-custom'] = Ractive.extend({
 	isolated: true,
 	data: { type: 'default' },
 	template: "<div class='panel panel-{{type}}' hidden='{{hidden}}'>{{yield}}</div>"
 })
-Ractive.components['panel-heading'] = Ractive.extend({isolated: true, template: "<div class='panel-heading'>{{yield}}</div>"})
+Ractive.components['panel-heading'] = Ractive.extend({isolated: true, data:{style:null}, template: "<div class='panel-heading' style='{{style}}'>{{yield}}</div>"})
 Ractive.components['panel-body'] = Ractive.extend({isolated: true, template: "<div class='panel-body'>{{yield}}</div>"})
 Ractive.components['panel-footer'] = Ractive.extend({isolated: true, template: "<div class='panel-footer'>{{yield}}</div>"})
 
