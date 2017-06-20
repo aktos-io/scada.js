fcbad9c7216c0b5b5f99dda6629384d2ac6a46ee
Cerem Cem ASLAN <cem@aktos.io>
2017-06-20 16:26:25 +0300

    cleaned up _merging_

--- _merging_/tools/broker.py
+++
@@ -1,9 +0,0 @@
-from aktos_dcs import *
-
-class Monitor(Actor):
-    def handle_IoMessage(self, msg):
-        print "monitor got io message:", msg['pin_name'], msg['val']
-
-ProxyActor(proxy_brokers="localhost:9012:9013", brokers="10.0.10.176:5012:5013")
-Monitor()
-wait_all()
--- _merging_/tools/virtual-io-server.py
+++
@@ -1,9 +0,0 @@
-from aktos_dcs import * 
-
-for i in range(2000):
-    pin_name = "vpin-%d" % i
-    VirtualIoActor(pin_name=pin_name)
-
-ProxyActor()
-print "Started virtual io-server..."
-wait_all()
