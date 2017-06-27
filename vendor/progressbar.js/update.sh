c895f6ee7dea8eda565fe6578a0357931ef317d3
Cerem Cem ASLAN <cem@aktos.io>
2017-06-27 22:02:27 +0300

    cleaned up progressbar files (as it is not needed anymore)

--- vendor/progressbar.js/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DIR=$(dirname "$(readlink -f "$0")")
-
-NODE_MODULES="$DIR/../../node_modules"
-echo "updating progressbar.js from this node modules"
-
-cp -av $NODE_MODULES/progressbar.js/dist/progressbar.min.js $DIR
