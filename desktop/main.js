ce3aaaef50a6b2afd9760d2a7ae236f324cf5ab1
Cerem Cem ASLAN <cem@aktos.io>
2017-07-09 14:09:33 +0300

    desktop app is started without developer tools

--- desktop/main.js
+++ desktop/main.js
@@ -26,7 +26,7 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
