50347cbe960aece151f848354a6ac89dde568692
Cerem Cem ASLAN <cem@aktos.io>
2018-10-04 02:26:26 +0300

    added watch.sh script for safer builds

---
+++ watch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+set -eu -o pipefail
+safe_source () { [[ ! -z ${1:-} ]] && source $1; _dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"; _sdir=$(dirname "$(readlink -f "$0")"); }; safe_source
+# end of bash boilerplate
+
+[[ -z ${1:-} ]] && { echo "first parameter must be the webapp name"; exit 1; }
+
+while :; do
+    gulp --webapp "$1"
+    echo "restarting gulp..."
+    sleep 1
+done
