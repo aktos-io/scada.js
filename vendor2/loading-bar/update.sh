fc57bbfa67ea0607328fcdb04ae3e033aff84a68
Cerem Cem ASLAN <cem@aktos.io>
2020-08-02 05:17:11 +0300

    updated loading-bar library

--- vendor2/loading-bar/update.sh
+++ vendor2/loading-bar/update.sh
@@ -3,7 +3,12 @@
 DIR=$(dirname "$(readlink -f "$0")")
 cd $DIR
 
-../download "https://raw.githubusercontent.com/loadingio/loading-bar/master/dist/loading-bar.js" &
-../download "https://raw.githubusercontent.com/loadingio/loading-bar/master/dist/loading-bar.css" &
+UPSTREAM="loadingio"
+CUSTOM="ceremcem"
+
+REPO=$CUSTOM
+
+../download "https://raw.githubusercontent.com/$REPO/loading-bar/master/dist/loading-bar.min.js" &
+../download "https://raw.githubusercontent.com/$REPO/loading-bar/master/dist/loading-bar.min.css" &
 wait
 
