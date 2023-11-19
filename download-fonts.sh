94589de9e7ba17d0abf966b8c92d8c62179bcd82
Cerem Cem ASLAN <cem@aktos.io>
2023-11-19 18:18:22 +0300

    fonts are now included offline. closes #208

--- download-fonts.sh
+++ download-fonts.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
+set -eu
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
 DIR=$(dirname "$(readlink -f "$0")")
 
-# Found in semantic.ui/site.min.css: @import url(https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic&subset=latin)
-# Changed with: ????????????
+# See API documentation: https://github.com/majodev/google-webfonts-helper#json-api
 
-mkdir -p $DIR/vendor2/02.semantic-ui/assets/fonts
-cd $DIR/vendor2/02.semantic-ui/assets/fonts
-curl -L -o f.zip "https://gwfh.mranftl.com/api/fonts/lato?download=zip&subsets=latin&variants=400,700,400italic,700italic" \
+# Found in semantic.ui/site.min.css: @import url(https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic&subset=latin)
+# Remove this "@import url(...);" part as it is included by $fonts_file.
+semanticui_dir=$DIR/vendor/02.semantic-ui/
+fonts_dir="$semanticui_dir/assets/fonts"
+fonts_file="fonts-of-site.min.css"
+
+cd "$semanticui_dir"
+if [ -f $fonts_file ]; then 
+    echo 
+    printf "${YELLOW}WARNING: $fonts_file exists, not downloading again. Remove it if you like to re-download.${NC}\n"
+    echo 
+else 
+    curl -o $fonts_file "https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic&subset=latin"
+fi
+mkdir -p $fonts_dir
+cd $fonts_dir
+# replaced 400 with regular
+# replaced 400italic with italic
+curl -L -o f.zip "https://gwfh.mranftl.com/api/fonts/lato?download=zip&subsets=latin&variants=regular,italic,700,700italic" \
     && unzip -o f.zip && rm f.zip
+
+printf "${GREEN}Edit $fonts_file accordingly.${NC}\n"
